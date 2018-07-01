77ecab4ed6dfc76d44776843bc20bbe85ba8c9bc
navjot singh <navjotsinghbe@gmail.com>
2018-07-01 17:27:52 +1000

    chat form

--- public/js/index.js
+++ public/js/index.js
@@ -8,11 +8,25 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (data) {
     console.log("new message",data);
+    var li = jQuery('<li></li>');
+    li.text(`${data.from}: ${data.text} `)
+    jQuery('#messages').append(li)
 });
 
-socket.emit('createMessage',  {
-    from : "nani ji",
-    text: "ki hal hai putti"
-},function(data){
-    console.log(data)
+// socket.emit('createMessage',  {
+//     from : "nani ji",
+//     text: "ki hal hai putti"
+// },function(data){
+//     console.log(data)
+// });
+
+jQuery('#chatForm').on('submit',function(e){
+    e.preventDefault();
+    
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function(err){
+        console.log(err)
+    });
 });
